b7113f2db8190cebe35bd1c4c8c89387ea35e52b
Roger Reyes <rreyes@pami.org.ar>
2019-10-10 15:51:29 -0300

    mejoras

--- notificadorSistemasAnmat/VerificarSistemasTraza/Program.cs
+++ notificadorSistemasAnmat/VerificarSistemasTraza/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace VerificarSistemasTraza
@@ -10,12 +11,21 @@ namespace VerificarSistemasTraza
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("El sistema esta en ejecución..", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
